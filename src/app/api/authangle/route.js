4b241aad4f9e9fc368eac741f20320f78aa345a5
Devan019 <devanchauhan012@gmail.com>
2025-03-23 15:27:31 +0530

    remove error of smart-api

--- src/app/api/authangle/route.js
+++ src/app/api/authangle/route.js
@@ -1,5 +1,3 @@
-import { SmartConnect } from "smartapi-nodejs";
-
 export async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
